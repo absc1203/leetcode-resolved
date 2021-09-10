89297e0a80bc9ced522747e33f485cc13969bb04
Andre <absc1203@gmail.com>
2021-09-10 10:22:47 -0300

    Two problems about arrays

---
+++ java/Concatenation of Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+      
+        int[] numsConcat = new int[nums.length * 2];
+        
+        for(int i = 0; i<nums.length; i++){
+            numsConcat[i] = nums[i];
+            numsConcat[i + nums.length] = nums[i];
+        }
+        
+        
+        return numsConcat;
+    }
+}
